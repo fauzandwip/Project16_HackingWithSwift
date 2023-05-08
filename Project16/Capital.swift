e0ff5e79ba80688936cfe10253b3a8c1fc4369d1
fauzandp <fauzan27dp@gmail.com>
2023-05-08 23:53:53 +0700

    Project16 | MapKit add anotation to some capital add anotation by CLLocationCoordinate2D longitude and latitude

---
+++ Project16/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Fauzan Dwi Prasetyo on 08/05/23.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String? = nil, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
